b014ce4827cc865007606d8967c0e70caabd0c30
VitonBaton <vitonbaton@yandex.ru>
2021-10-30 22:00:01 +0300

    Add Order model

---
+++ ITechArtPizzaDelivery.Domain/Models/Order.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITechArtPizzaDelivery.Domain.Models
+{
+    public class Order
+    {
+        public long Id { get; set; }
+        public User User { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        [ForeignKey("DeliveryId")]
+        public Delivery Delivery { get; set; }
+        [Required]
+        [ForeignKey("PaymentId")]
+        public Payment Payment { get; set; }
+        [Required]
+        public string Comment { get; set; }
+        public Promocode Promocode { get; set; }
+        public DateTime CreateAt { get; set; }
+        [Column(TypeName = "decimal(5, 2)")]
+        [Required]
+        public decimal Price { get; set; }
+        public Status Status { get; set; }
+    }
+}
