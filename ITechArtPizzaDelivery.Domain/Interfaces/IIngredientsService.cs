be0ed57b65daed0feabfcec95ce7f18690d372ec
VitonBaton <vitonbaton@yandex.ru>
2021-10-30 22:15:20 +0300

    Add Ingredients service

---
+++ ITechArtPizzaDelivery.Domain/Interfaces/IIngredientsService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ITechArtPizzaDelivery.Domain.Models;
+
+namespace ITechArtPizzaDelivery.Domain.Services
+{
+    public interface IIngredientsService
+    {
+        Task<List<Ingredient>> GetAll();
+        Task<Ingredient> GetById(long id);
+        Task<Ingredient> Post(Ingredient ingredient);
+    }
+}
---
+++ ITechArtPizzaDelivery.Domain/Services/IngredientsService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ITechArtPizzaDelivery.Domain.Interfaces;
+using ITechArtPizzaDelivery.Domain.Models;
+
+namespace ITechArtPizzaDelivery.Domain.Services
+{
+    public class IngredientsService : IIngredientsService
+    {
+        private readonly IIngredientsRepository _ingredientsRepository;
+
+        public IngredientsService(IIngredientsRepository ingredientsRepository)
+        {
+            _ingredientsRepository = ingredientsRepository;
+        }
+
+        public Task<List<Ingredient>> GetAll()
+        {
+            return _ingredientsRepository.GetAll();
+        }
+
+        public Task<Ingredient> GetById(long id)
+        {
+            return _ingredientsRepository.GetById(id);
+        }
+        public Task<Ingredient> Post(Ingredient ingredient)
+        {
+            return _ingredientsRepository.Post(ingredient);
+        }
+    }
+}
