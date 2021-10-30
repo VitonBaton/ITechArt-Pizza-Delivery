0620f6743e8a2ac8fbf631666e7eafd50823e7dd
VitonBaton <vitonbaton@yandex.ru>
2021-10-30 21:45:50 +0300

    Add Cart model

---
+++ ITechArtPizzaDelivery.Domain/Models/Cart.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITechArtPizzaDelivery.Domain.Models
+{
+    public class Cart
+    {
+        public long Id { get; set; }
+        [Required]
+        public int PizzaCount { get; set; }
+        [Required]
+        public Pizza Pizza { get; set; }
+        [Required]
+        public User Customer { get; set; }
+        public Order Order { get; set; }
+    }
+}
