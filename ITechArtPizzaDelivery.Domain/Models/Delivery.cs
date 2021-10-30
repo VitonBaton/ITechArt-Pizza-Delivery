ffda6bd0cb02f080d1a8ca6fb7513884f4c55f61
VitonBaton <vitonbaton@yandex.ru>
2021-10-30 21:46:04 +0300

    Add Delivery model

---
+++ ITechArtPizzaDelivery.Domain/Models/Delivery.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITechArtPizzaDelivery.Domain.Models
+{
+    public class Delivery
+    {
+        public long Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        [Column(TypeName = "decimal(5, 2)")]
+        public decimal Price { get; set; }
+        public Order Order { get; set; }
+    }
+}
