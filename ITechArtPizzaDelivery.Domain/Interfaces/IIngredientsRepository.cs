6ed24a41b1c1dfae78ae40a24b578318b092caa5
VitonBaton <vitonbaton@yandex.ru>
2021-10-30 22:10:06 +0300

    Add Ingredients repository

---
+++ ITechArtPizzaDelivery.Domain/Interfaces/IIngredientsRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ITechArtPizzaDelivery.Domain.Models;
+
+namespace ITechArtPizzaDelivery.Domain.Interfaces
+{
+    public interface IIngredientsRepository
+    {
+        public Task<List<Ingredient>> GetAll();
+        public Task<Ingredient> GetById(long id);
+        public Task<Ingredient> Post(Ingredient ingredient);
+    }
+}
---
+++ ITechArtPizzaDelivery.Infrastructure/Repositories/EFRepositories/IngredientsRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ITechArtPizzaDelivery.Domain.Interfaces;
+using ITechArtPizzaDelivery.Domain.Models;
+using ITechArtPizzaDelivery.Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITechArtPizzaDelivery.Infrastructure.Repositories.EFRepositories
+{
+    public class IngredientsRepository : IIngredientsRepository
+    {
+        private readonly PizzaDeliveryContext _dbContext;
+
+        public IngredientsRepository(PizzaDeliveryContext context)
+        {
+            _dbContext = context;
+        }
+
+        public async Task<Ingredient> GetById(long id)
+        {
+            return await _dbContext.Ingredients.FindAsync(id);
+        }
+
+        public async Task<Ingredient> Post(Ingredient ingredient)
+        {
+            _dbContext.Ingredients.Add(ingredient);
+            await _dbContext.SaveChangesAsync();
+            return ingredient;
+        }
+
+        public async Task<List<Ingredient>> GetAll()
+        {
+            return await _dbContext.Ingredients.ToListAsync();
+        }
+    }
+}
