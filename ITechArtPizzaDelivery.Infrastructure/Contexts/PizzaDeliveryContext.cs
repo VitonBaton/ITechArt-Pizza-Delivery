b4f1a9017c6cd727dc27b70721095438c78cb70b
VitonBaton <vitonbaton@yandex.ru>
2021-10-30 22:22:34 +0300

    Add database context

---
+++ ITechArtPizzaDelivery.Infrastructure/Contexts/PizzaDeliveryContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ITechArtPizzaDelivery.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITechArtPizzaDelivery.Infrastructure.Contexts
+{
+    public class PizzaDeliveryContext :DbContext
+    {
+        public DbSet<Pizza> Pizzas { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Delivery> Deliveries { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Promocode> Promocodes { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+
+        public PizzaDeliveryContext(DbContextOptions<PizzaDeliveryContext> options) 
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+    }
+}
