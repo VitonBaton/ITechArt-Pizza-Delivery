5b5fd1e8b079115066287a25b6c5c777ab087c1e
VitonBaton <vitonbaton@yandex.ru>
2021-11-09 20:11:43 +0300

    Add promocodes controller and repository

---
+++ ITechArtPizzaDelivery.Domain/Interfaces/IPromocodesRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ITechArtPizzaDelivery.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITechArtPizzaDelivery.Infrastructure.Repositories.EFRepositories
+{
+    public interface IPromocodesRepository
+    {
+        Task<Promocode> Post(Promocode promocode);
+        Task<List<Promocode>> GetAll();
+        Task<Promocode> GetById(long id);
+        Task<IActionResult> DeleteById(long id);
+    }
+}
---
+++ ITechArtPizzaDelivery.Domain/Interfaces/IPromocodesService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ITechArtPizzaDelivery.Domain.Models;
+
+namespace ITechArtPizzaDelivery.Domain.Services
+{
+    public interface IPromocodesService
+    {
+        Task<List<Promocode>> GetAll();
+        Task<Promocode> GetById(long id);
+        Task<Promocode> Post(Promocode promocode);
+        Task DeleteById(long id);
+    }
+}
