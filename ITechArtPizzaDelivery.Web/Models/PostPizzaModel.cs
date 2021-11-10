34980b5ac162414360bb5730e6356c63a24e19f1
VitonBaton <vitonbaton@yandex.ru>
2021-11-10 16:15:30 +0300

    Add model for posting pizza

---
+++ ITechArtPizzaDelivery.Web/Models/PostPizzaModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITechArtPizzaDelivery.Web.Models
+{
+    public class PostPizzaModel
+    {
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public decimal Price { get; set; }
+        public long[] IngredientsId { get; set; }
+    }
+}
