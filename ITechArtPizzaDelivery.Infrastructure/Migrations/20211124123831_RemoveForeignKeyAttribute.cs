fc901cb978d471f10a151cbe213284eb010a62a6
VitonBaton <vitonbaton@yandex.ru>
2021-11-24 15:45:32 +0300

    Remove foreign key attribute from models

---
+++ ITechArtPizzaDelivery.Infrastructure/Migrations/20211124123831_RemoveForeignKeyAttribute.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ITechArtPizzaDelivery.Infrastructure.Migrations
+{
+    public partial class RemoveForeignKeyAttribute : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_DeliveryId",
+                table: "Orders");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Promocodes",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Promocodes_Name",
+                table: "Promocodes",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_DeliveryId",
+                table: "Orders",
+                column: "DeliveryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Promocodes_Name",
+                table: "Promocodes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_DeliveryId",
+                table: "Orders");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Promocodes",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_DeliveryId",
+                table: "Orders",
+                column: "DeliveryId",
+                unique: true);
+        }
+    }
+}
