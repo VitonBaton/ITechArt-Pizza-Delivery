8d9e9c01638b3d77ea239680e662b57e97780f6a
VitonBaton <vitonbaton@yandex.ru>
2021-11-24 16:15:06 +0300

    Remove unnecessary comments

--- ITechArtPizzaDelivery.Domain/Interfaces/ICartService.cs
+++ ITechArtPizzaDelivery.Domain/Interfaces/ICartService.cs
@@ -10,6 +10,5 @@ namespace ITechArtPizzaDelivery.Domain.Interfaces
         Task<List<CartPizza>> GetPizzasFromCart(int customerId);
         Task ChangeAmountOfPizza(int pizzaId, int customerId, int pizzasCount);
         Task DeleteByPizzaId(int customerId, int pizzaId);
-        //Task<Cart> GetPizzaById(int customerId, int pizzaId);
     }
 }
