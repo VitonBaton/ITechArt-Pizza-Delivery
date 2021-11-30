264a79631087b317e393d5dc968c5dbcc0db6a25
VitonBaton <vitonbaton@yandex.ru>
2021-11-30 21:04:59 +0300

    Add orders repository

---
+++ ITechArtPizzaDelivery.Domain/Interfaces/IOrdersRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ITechArtPizzaDelivery.Domain.Models;
+
+namespace ITechArtPizzaDelivery.Domain.Interfaces
+{
+    public interface IOrdersRepository
+    {
+        Task<Order> CreateNewOrder(int customerId, string address, int deliveryId, int paymentId, string comment);
+        Task CancelOrder(int orderId);
+        Task<List<Order>> GetCustomerOrders(int customerId);
+        Task AddPromocodeToOrder(int orderId, string promocodeName);
+        Task<List<User>> GetAllUsersAndOrders();
+    }
+}
