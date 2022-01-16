b4ebe7c296db1001b65e82488ad3c68f7cafb6d2
VitonBaton <vitonbaton@yandex.ru>
2022-01-17 01:45:47 +0300

    Add roles seeding

---
+++ ITechArtPizzaDelivery.Infrastructure/DataSeeds/RolesSeeds.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+
+namespace ITechArtPizzaDelivery.Infrastructure.DataSeeds
+{
+    public class RolesSeeds
+    {
+        public static readonly List<IdentityRole<int>> Roles = new()
+        {
+            new IdentityRole<int>
+            {
+                Id = 1,
+                Name = "Admin",
+                NormalizedName = "Admin".ToUpper()
+            }
+        };
+    }
+}
