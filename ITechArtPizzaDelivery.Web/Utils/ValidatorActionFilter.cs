dcb39cb456732c3e6acde1f056604bd9e2e1a0bf
VitonBaton <vitonbaton@yandex.ru>
2022-01-26 01:44:12 +0300

    Add ValidationActionFilter for checking models to validity

---
+++ ITechArtPizzaDelivery.Web/Utils/ValidatorActionFilter.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ITechArtPizzaDelivery.Web.Utils
+{
+    public class ValidatorActionFilter : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                context.Result = new BadRequestObjectResult(context.ModelState);
+            }
+
+            await next();
+        }
+    }
+}
